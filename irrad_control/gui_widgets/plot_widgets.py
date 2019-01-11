e26e6bb167c22423bb33a49b137098ffa60fa09c
leloup314 <s6plwolf@uni-bonn.de>
2019-01-11 07:31:43 +0100

    ENH: add gui_widgets

---
+++ irrad_control/gui_widgets/plot_widgets.py
@@ -0,0 +1,17 @@
+import pyqtgraph as pg
+
+
+class SEMCurrentGraph(pg.PlotItem):
+    pass
+
+
+class BeamCurrentGraph(pg.PlotItem):
+    pass
+
+
+class BeamPositionGraph(pg.PlotItem):
+    pass
+
+
+class FluenceMap(pg.ViewBox):
+    pass
