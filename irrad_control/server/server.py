125906bb68350428c7285f4debe09ffd04180ca5
leloup314 <s6plwolf@uni-bonn.de>
2019-01-17 13:37:12 +0100

    ENH: add server_manager.py; MAINT: remove scripts folder, rename

---
+++ irrad_control/server/server.py
@@ -0,0 +1,162 @@
+import os
+import zmq
+import sys
+import time
+import yaml
+import multiprocessing
+import threading
+import logging
+import argparse
+import psutil
+from collections import Iterable, defaultdict, OrderedDict
+from zmq.log import handlers
+from adc.ADS1256_definitions import *
+from adc.pipyadc import ADS1256
+
+
+# ADS1256 data rates and number of averages ber data rate
+ads1256 = OrderedDict()
+
+# Data rate in samples per second
+ads1256_drates = OrderedDict([(30000, DRATE_30000), (15000, DRATE_15000), (7500, DRATE_7500), (3750, DRATE_3750),
+                              (2000, DRATE_2000), (1000, DRATE_1000), (500, DRATE_500), (100, DRATE_100),
+                              (60, DRATE_60), (50, DRATE_50), (30, DRATE_30), (25, DRATE_25),
+                              (15, DRATE_15), (10, DRATE_10), (5, DRATE_5), (2.5, DRATE_2_5)])
+
+
+class IrradServer(multiprocessing.Process):
+
+    def __init__(self, cmd_port):
+        super(IrradServer, self).__init__()
+        
+        # Process info
+        #self.process = psutil.Process(self.ident)
+        
+        self._send_data = True
+        self._recv_cmds = True
+        
+        self.cmd_port = cmd_port
+        
+        # List of known commands
+        self._cmds = ['setup_zmq']
+        
+        self.irrad_config = None
+        
+    def _tcp_addr(self, port, ip='*'):
+        """Creates string of complete tcp address which sockets can bind to"""
+        return 'tcp://{}:{}'.format(ip, port)
+
+    def _init_zmq(self):
+        """Setup of all 0MQ related sockets"""
+
+        # create context; threadsafe, sockets though have to be created within the thread of their context
+        self.context = zmq.Context()
+        
+        # Data publisher
+        self.data_pub = self.context.socket(zmq.PUB)
+        self.data_pub.set_hwm(10)  # drop data if too slow
+        #self.data_pub.bind(self._tcp_addr(self.tcp_config['port']['raw_data']))
+        
+        # Log publisher
+        self.log_pub = self.context.socket(zmq.PUB)
+        #self.log_pub.bind(self._tcp_addr(self.tcp_config['port']['log']))
+        
+        # Reply publisher
+        self.reply_pub = self.context.socket(zmq.PUB)
+        
+        # Command receiver
+        self.cmd_sub = self.context.socket(zmq.SUB)
+        self.cmd_sub.connect(self._tcp_addr(self.cmd_port, ip=self.host_ip))
+        self.cmd_sub.setsockopt(zmq.SUBSCRIBE, '')
+        
+    def _setup_server(self, irrad_config):
+        
+        self.irrad_config = irrad_config
+        self.adc_name = irrad_config['daq'].keys()[0]
+        self.daq_config = irrad_config['daq'][self.adc_name]
+        self.tcp_config = irrad_config['tcp']
+        
+        # Bind pubs
+        self.data_pub.bind(self._tcp_addr(self.tcp_config['port']['raw_data']))
+        self.log_pub.bind(self._tcp_addr(self.tcp_config['port']['log']))
+        self.reply_pub.bind(self._tcp_addr(self.tcp_config['port']['reply']))
+        
+        # Setup logging
+        self._setup_logging()
+        
+        # Setup adc
+        self._setup_adc()
+        
+        data_thread = threading.Thread(target=self.send_data)
+        data_thread.start()
+        
+        self._now_its_cool = True
+
+    def _setup_logging(self):
+
+        numeric_level = getattr(logging, self.irrad_config['log']['level'].upper(), None)
+        if not isinstance(numeric_level, int):
+            raise ValueError('Invalid log level: {}'.format(self.irrad_config['log']['level']))
+        logging.basicConfig(level=numeric_level)
+
+        # Create logging publisher first
+        handler = handlers.PUBHandler(self.log_pub)
+        logging.getLogger().addHandler(handler)
+        
+    def _setup_adc(self):
+        
+        self.adc = ADS1256()
+        self.adc.drate = ads1256_drates[self.daq_config['sampling_rate']]
+        
+        # self-calibration
+        self.adc.cal_self()
+    
+        # channels TODO: represent not only positive channels
+        self._all_channels = [ch_i|NEG_AINCOM for ch_i in (POS_AIN0, POS_AIN1, POS_AIN2, POS_AIN3,
+                                                           POS_AIN4, POS_AIN5, POS_AIN6, POS_AIN7)]
+        
+        self.adc_channels = [self._all_channels[i] for i in range(len(self.daq_config['channels']))]
+        
+    def recv_cmd(self):
+        """Receiving commands from tcp://*:port"""
+        
+        while self._recv_cmds:
+            # Cmd must be dict with command as 'cmd' key and 'args', 'kwargs' keys
+            cmd_dict = self.cmd_sub.recv_json()
+            if 'cmd' not in cmd_dict:
+                logging.error("Command must be dictionary with actual command in cmd_dict['cmd']!")
+                self.reply_pub.send_json({'reply': 'Error'})
+                continue
+            self.handle_cmd(cmd_dict)
+            
+    def handle_cmd(self, cmd_dict):
+        
+        cmd = cmd_dict['cmd']
+        cmd_data = None if 'data' not in cmd_dict else cmd_dict['data']
+        
+        if cmd == 'setup_server' and not self._now_its_cool:
+            self._setup_server(cmd_data)
+            
+        if cmd == 'herro':
+            self.reply_pub.send_json({'reply': 'Herro'})
+        
+    def send_data(self):
+        
+        while self._send_data:
+            
+            raw_data = self.adc.read_sequence(self.adc_channels)
+            _meta = {'timestamp': time.time(), 'name': self.adc_name, 'type': 'raw'}
+            _data = dict([(self.daq_config['channels'][i], raw_data[i] * self.adc.v_per_digit) for i in range(len(raw_data))])
+            
+            self.data_pub.send_json({'meta': _meta, 'data': _data})
+        
+    def run(self):
+        # Main process runs command receive loop
+        self._init_zmq()
+        self.recv_cmd()
+            
+if __name__ == '__main__':
+    
+    port = sys.argv[1]
+    irrad_server = IrradServer(port) #'5400')
+    irrad_server.start()
---
+++ irrad_control/utils/server_manager.py
@@ -0,0 +1,115 @@
+import os
+import time
+import logging
+import threading
+import paramiko
+import subprocess
+import tarfile
+from irrad_control import package_path, server_path
+
+
+class ServerManager(object):
+    """
+    Class to enable communication via SSH2 implementation of the paramiko library between host PC and
+    Raspberry Pi server. This class implements methods to prepare, start and monitor the server process
+    which handles the data acquisition and stage. 
+    """
+    
+    def __init__(self, hostname, username='pi'):
+        super(ServerManager, self).__init__()
+        
+        # Input
+        self.hname = hostname
+        self.uname = username
+        
+        # Setup SSH client and connect to server
+        self.client = paramiko.SSHClient()
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        
+        logging.info('Connecting to server {}@{}...'.format(username, hostname))
+        
+        try:
+            self.client.connect(hostname=hostname, username=username)
+        except (paramiko.BadHostKeyException, paramiko.AuthenticationException, paramiko.SSHException) as e:
+            
+            if type(e) in (paramiko.BadHostKeyException):
+                msg = "Server's host key could not be verified. Try adding key via ssh-keygen and ssh-copy-id!"
+                raise e(msg)
+            else:
+                raise e
+            
+        logging.info('Successfully connected to server {}@{}!'.format(username, hostname))
+        
+    def prepare_server(self):
+        """Prepares the server by copying the all neccesarry files to it and executing check scripts"""
+        
+        # Server locations and files
+        self.remote_path = '/home/{}/server/'.format(self.uname)
+        self.archive = 'server.tgz'
+        
+        logging.info('Creating archive {} ...'.format(self.archive))
+        
+        # Removing old server folder and making new
+        self.exec_cmd('rm -rf {}'.format(self.remote_path))
+        self.exec_cmd('mkdir {}'.format(self.remote_path))
+        
+        archive_path = os.path.join(package_path, self.archive)
+        
+        with tarfile.open(archive_path, 'w:gz') as tgz:
+            tgz.add(server_path, arcname=os.path.sep)
+           
+        logging.info('Copying {} to {}@{}...'.format(self.archive, self.uname, self.hname))
+        
+        self.copy_to_server(archive_path, self.remote_path + self.archive)
+        
+        logging.info('Unpacking server.tgz on server...')
+        
+        self.exec_cmd('cd {}; tar -xzf server.tgz'.format(self.remote_path, self.uname))
+        
+        logging.info('Done')
+        
+        # Remove archive on host PC
+        self._call_subprocess(['rm', archive_path])
+        
+        # Run script to determine wheter server Pi has miniconda and all packages installed
+        self.exec_cmd('bash {}/setup_server.sh'.format(self.remote_path), log_stdout=True)
+        
+    def start_server_process(self, port):
+        
+        logging.info('Starting server process listening to port {}...'.format(port))
+        
+        self.exec_cmd('source /home/pi/miniconda/bin/activate; nohup python {}/server.py {} &'.format(self.remote_path, port))
+        
+    def _call_subprocess(self, cmd_list):
+        """Calls subprocess"""
+        p = subprocess.Popen(cmd_list)
+        
+        while p.poll() is None:
+            time.sleep(0.1)
+            
+    def exec_cmd(self, cmd, log_stdout=False):
+        """Execute command on server using paramikos SSH implementation"""
+        
+        # Execute; this is non-blocking so we have to wait until cmd has been transmitted to server before closing
+        stdin, stdout, stderr = self.client.exec_command(cmd)
+        
+        # No writing to stdin and stdout happens
+        stdin.close()
+        stdout.channel.shutdown_write()
+        
+        if log_stdout:
+            while not stdout.channel.exit_status_ready():
+                msg = stdout.readline().strip()
+                if msg:
+                    logging.info(msg)
+                
+        stdout.close()
+        stderr.close()
+            
+    def copy_to_server(self, local_filepath, remote_filepath):
+        """Copy local file at local_filepath to server at remote_filepath"""
+        
+        sftp = self.client.open_sftp()
+        sftp.put(local_filepath, remote_filepath)
+        sftp.close()
+        
