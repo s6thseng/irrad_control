339a5c48f060f4c688d9038050904fa25cd74537
leloup314 <s6plwolf@uni-bonn.de>
2019-01-17 02:33:03 +0100

    ENH: add to plot widgets

--- irrad_control/gui_widgets/plot_widgets.py
+++ irrad_control/gui_widgets/plot_widgets.py
@@ -1,6 +1,8 @@
 import logging
 import pyqtgraph as pg
 import numpy as np
+from PyQt5 import QtWidgets, QtCore
+from collections import OrderedDict
 from irrad_control import roe_output
 
 # Matplotlib first 8 default colors
@@ -8,14 +10,103 @@ MPL_COLORS = [(31, 119, 180), (255, 127, 14), (44, 160, 44), (214, 39, 40),
               (148, 103, 189), (140, 86, 75), (227, 119, 194), (127, 127, 127)]
 
 
-class RawDataGraph(pg.PlotWidget):
+class PlotWindow(QtWidgets.QMainWindow):
+    """Window which only shows a PlotWidget as its central widget."""
+
+    closeWin = QtCore.pyqtSignal()
+
+    def __init__(self, plot, parent=None):
+        super(PlotWindow, self).__init__(parent)
+
+        self.plot = plot
+        self.setWindowTitle(type(plot).__name__)
+        self.screen = QtWidgets.QDesktopWidget().screenGeometry()
+        self.setMinimumSize(0.75 * self.screen.width(), 0.75 * self.screen.height())
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+
+        self.setCentralWidget(self.plot)
+
+    def closeEvent(self, _):
+        self.closeWin.emit()
+        self.close()
+
+
+class PlotWrapperWidget(QtWidgets.QWidget):
+    """Widget that wraps PlotWidgets and implements some additional features like checkboxes that allow
+    selecting the plots which are shown. Also adds button to show the respective PlotWidget in a QMainWindow"""
+
+    def __init__(self, plot=None, parent=None):
+        super(PlotWrapperWidget, self).__init__(parent=parent)
+
+        # Layout
+        self.setLayout(QtWidgets.QVBoxLayout())
+        self.plot = plot
+
+        # Layout for checkboxes which allow to show/hide curves in PlotWidgets
+        self.sub_layout = QtWidgets.QHBoxLayout()
+
+        if self.plot is not None:
+            self._setup_widget()
+
+    def _setup_widget(self):
+
+        has_show_data_method = hasattr(self.plot, 'show_data')
+
+        # Create checkboxes in order to show/hide curves in plots
+        if has_show_data_method and hasattr(self.plot, 'curves'):
+            self.sub_layout.addWidget(QtWidgets.QLabel('Show curve:'))
+            for curve in self.plot.curves:
+                checkbox = QtWidgets.QCheckBox(curve)
+                checkbox.setChecked(True)
+                checkbox.stateChanged.connect(lambda v, n=checkbox.text(): self.plot.show_data(n, bool(v)))
+                self.sub_layout.addWidget(checkbox)
+        else:
+            logging.warning("{} has no 'show_data' method. Please implement it!".format(type(self.plot).__name__))
+
+        has_update_period_method = hasattr(self.plot, 'update_period')
+
+        # Whenever x axis is time add spinbox to change time period for which data is shown
+        if has_update_period_method:
+            spinbox = QtWidgets.QSpinBox()
+            spinbox.setRange(1, 3600)
+            spinbox.setValue(self.plot._period)
+            spinbox.setPrefix('Time period: ')
+            spinbox.setSuffix(' s')
+            spinbox.valueChanged.connect(lambda v: self.plot.update_period(v))
+            self.sub_layout.addWidget(spinbox)
+
+        self.btn = QtWidgets.QPushButton()
+        self.btn.setIcon(self.btn.style().standardIcon(QtWidgets.QStyle.SP_TitleBarMaxButton))
+        self.btn.setToolTip('Open plot in window')
+        self.btn.setFixedSize(30, 30)
+        self.btn.clicked.connect(self.move_to_win)
+        self.btn.clicked.connect(lambda: self.btn.setEnabled(False))
+        self.sub_layout.addStretch()
+        self.sub_layout.addWidget(self.btn)
+
+        self.layout().addLayout(self.sub_layout)
+        self.layout().addStretch()
+        self.layout().addWidget(self.plot)
+
+    def set_plot(self, plot):
+        self.plot = plot
+        self._setup_widget()
+
+    def move_to_win(self):
+        pw = PlotWindow(plot=self.plot, parent=self)
+        pw.closeWin.connect(lambda: self.layout().addWidget(self.plot))
+        pw.closeWin.connect(lambda: self.btn.setEnabled(True))
+        pw.show()
+
+
+class RawDataPlot(pg.PlotWidget):
     """
     Plot for displaying the raw data of all channels of the respective ADC over time.
     Data is displayed in rolling manner over period seconds
     """
 
     def __init__(self, daq_config, period=10, parent=None):
-        super(RawDataGraph, self).__init__(parent=parent)
+        super(RawDataPlot, self).__init__(parent=parent)
 
         # Init class attributes
         self.daq_config = daq_config
@@ -49,8 +140,8 @@ class RawDataGraph(pg.PlotWidget):
         self.plt.setRange(yRange=[-5., 5.])
         self.plt.setLimits(xMax=0)
 
-        # Make dict of curves
-        self.curves = dict([(ch, pg.PlotCurveItem(pen=MPL_COLORS[i%len(MPL_COLORS)])) for i, ch in enumerate(self.channels)])
+        # Make OrderedDict of curves
+        self.curves = OrderedDict([(ch, pg.PlotCurveItem(pen=MPL_COLORS[i%len(MPL_COLORS)])) for i, ch in enumerate(self.channels)])
 
         # Make legend entries for curves
         self.legend = pg.LegendItem(offset=(80, -50))
@@ -67,14 +158,16 @@ class RawDataGraph(pg.PlotWidget):
 
         timestamp = _meta['timestamp']
 
+        if 'data_rate' in _meta:
+            self._drate = _meta['data_rate']
+
         # Get data rate from data in order to set time axis
         if self._time is None:
-
             if 'data_rate' in _meta:
                 self._drate = _meta['data_rate']
                 shape = int(round(self._drate) * self._period + 1)
                 self._time = np.zeros(shape=shape)
-                self._data = dict([(ch, np.zeros(shape=shape)) for i, ch in enumerate(self.channels)])
+                self._data = OrderedDict([(ch, np.zeros(shape=shape)) for i, ch in enumerate(self.channels)])
 
             self.plt.setTitle(_meta['name'] + ' raw data')
 
@@ -126,7 +219,7 @@ class RawDataGraph(pg.PlotWidget):
 
         # Create new data and time
         shape = int(round(self._drate) * self._period + 1)
-        new_data = dict([(ch, np.zeros(shape=shape)) for i, ch in enumerate(self.channels)])
+        new_data = OrderedDict([(ch, np.zeros(shape=shape)) for i, ch in enumerate(self.channels)])
         new_time = np.zeros(shape=shape)
 
         # Check whether new time and data hold more or less indices
@@ -159,28 +252,32 @@ class BeamPositionItem:
     def __init__(self, color, name, intersect_symbol=None):
 
         # Init items needed
-        self.h_line = pg.InfiniteLine(angle=0)
-        self.v_line = pg.InfiniteLine(angle=90)
+        self.h_shift_line = pg.InfiniteLine(angle=90)
+        self.v_shift_line = pg.InfiniteLine(angle=0)
         self.intersect = pg.ScatterPlotItem()
 
         # Drawing style
-        self.h_line.setPen(color=color, style=pg.QtCore.Qt.DashLine, width=2)
-        self.v_line.setPen(color=color, style=pg.QtCore.Qt.DashLine, width=2)
+        self.h_shift_line.setPen(color=color, style=pg.QtCore.Qt.SolidLine, width=2)
+        self.v_shift_line.setPen(color=color, style=pg.QtCore.Qt.SolidLine, width=2)
         self.intersect.setPen(color=color, style=pg.QtCore.Qt.SolidLine)
+        self.intersect.setBrush(color=color)
         self.intersect.setSymbol('o' if intersect_symbol is None else intersect_symbol)
-        self.intersect.setSize(2)
+        self.intersect.setSize(10)
 
         # Items
-        self.items = [self.h_line, self.v_line, self.intersect]
+        self.items = [self.h_shift_line, self.v_shift_line,self.intersect]
         self.legend = None
         self.plotitem = None
         self.name = name
 
     def set_position(self, x, y):
 
-        self.h_line.setPos(x)
-        self.v_line.setPos(y)
-        self.intersect.setData([x], [y])
+        _x = x if x is not None else self.h_shift_line.value()
+        _y = y if y is not None else self.v_shift_line.value()
+
+        self.h_shift_line.setValue(_x)
+        self.v_shift_line.setValue(_y)
+        self.intersect.setData([_x], [_y])
 
     def set_plotitem(self, plotitem):
         self.plotitem = plotitem
@@ -222,7 +319,7 @@ class BeamPositionItem:
             else:
                 plotitem.removeItem(item)
 
-    def remove_from_legend(self, label, legend=None):
+    def remove_from_legend(self, label=None, legend=None):
 
         if legend is None and self.legend is None:
             raise ValueError('LegendItem needed!')
@@ -235,13 +332,13 @@ class BeamPositionItem:
             legend.removeItem(_lbl)
 
 
-class BeamPositionGraph(pg.PlotWidget):
+class BeamPositionPlot(pg.PlotWidget):
     """
     Plot for displaying the beam position. The position is displayed from analog and digital data if available.
     """
 
     def __init__(self, daq_config, parent=None):
-        super(BeamPositionGraph, self).__init__(parent=parent)
+        super(BeamPositionPlot, self).__init__(parent=parent)
 
         # Init class attributes
         self.daq_config = daq_config
@@ -250,8 +347,9 @@ class BeamPositionGraph(pg.PlotWidget):
         self.ro_scale = daq_config['ro_scale']
         self.adc = None
 
-        # Make curves
-        self.plots = ('Digital', 'Analog')
+        # Possible curves
+        self.d_pos = 'digital_pos'
+        self.a_pos = 'analog_pos'
 
         # Setup the main plot
         self._setup_plot()
@@ -266,57 +364,70 @@ class BeamPositionGraph(pg.PlotWidget):
         self.plt.showGrid(x=True, y=True, alpha=0.66)
         self.plt.setRange(xRange=[-5., 5.], yRange=[-5., 5.])
         self.plt.setLimits(xMax=10, xMin=-10, yMax=10, yMin=-10)
-
-        self.curves = {}
-        for plot in self.plots:
-            curves = []
-            pg.ScatterPlotItem()
-
-        # Make dict of curves
-        self.curves = dict([(ch, pg.PlotCurveItem(pen=MPL_COLORS[i % len(MPL_COLORS)])) for i, ch in enumerate(self.channels)])
-
-        # Make legend entries for curves
+        self.plt.hideButtons()
+        self.plt.addLine(x=0., pen={'color': 'w', 'style': pg.QtCore.Qt.DashLine})
+        self.plt.addLine(y=0., pen={'color': 'w', 'style': pg.QtCore.Qt.DashLine})
         self.legend = pg.LegendItem(offset=(80, -50))
         self.legend.setParentItem(self.plt)
 
+        self.curves = {}
+
+        # Check which channel types are present and fill curves
+        if all(t in self.ro_types for t in ('sem_left', 'sem_right', 'sem_up', 'sem_down')):
+            self.curves[self.d_pos] = BeamPositionItem(color=MPL_COLORS[0], name=self.d_pos)
+
+        if all(t in self.ro_types for t in ('sem_h_shift', 'sem_v_shift')):
+            self.curves[self.a_pos] = BeamPositionItem(color=MPL_COLORS[1], name=self.a_pos)
+
         # Show data and legend
-        for ch in self.channels:
-            self.show_data(ch)
+        if self.curves:
+            for curve in self.curves:
+                self.curves[curve].set_legend(self.legend)
+                self.curves[curve].set_plotitem(self.plt)
+                self.show_data(curve)
 
-    def _mk_pos_curves(self):
+    def set_data(self, data):
 
-        pos_curves = {}
-        for i, plt in enumerate(self.plots):
+        # Meta data and data
+        _meta, _data = data['meta'], data['data']
 
-            # Each position plot consist of a reticle (two crossed lines) and a circle at their intersect
-            h_line = pg.InfiniteLine(angle=0)
-            v_line = pg.InfiniteLine(angle=90)
-            intersect = pg.ScatterPlotItem()
+        if self.d_pos in self.curves:
+            # Get indices
+            l, r, u, d = (self.ro_types.index(x) for x in ('sem_left', 'sem_right', 'sem_up', 'sem_down'))
 
-            # Drawing stuff
-            h_line.setPen(color=MPL_COLORS[i], style=pg.QtCore.Qt.DashLine, width=2)
-            v_line.setPen(color=MPL_COLORS[i], style=pg.QtCore.Qt.DashLine, width=2)
-            intersect.setPen(color=MPL_COLORS[i], style=pg.QtCore.Qt.SolidLine)
-            intersect.setSymbol('o')
-            intersect.setSize(2)
+            h_shift = self._calc_shift(_data[self.channels[l]], _data[self.channels[r]], m='h')
+            v_shift = self._calc_shift(_data[self.channels[u]], _data[self.channels[d]], m='v')
 
-            pos_items = {'h_line': pg.InfiniteLine(), 'v_line': pg.InfiniteLine(), 'intersect': pg.ScatterPlotItem()}
+            self.curves[self.d_pos].set_position(h_shift, v_shift)
+
+        if self.a_pos in self.curves:
+            # Get indices
+            h, v = (self.ro_types.index(x) for x in ('sem_h_shift', 'sem_v_shift'))
+            h_shift, v_shift = _data[self.channels[h]], _data[self.channels[v]]
+            self.curves[self.a_pos].set_position(h_shift, v_shift)
+
+    def _calc_shift(self, a, b, m='h'):
+
+        try:
+            res = (a - b) / (a + b)
+        except ZeroDivisionError:
+            res = None
+
+        # Horizontally, if we are shifted to the left the graph should move to the left, therefore * -1
+        return res if res is None else -1 * res if m == 'h' else res
 
     def show_data(self, curve, show=True):
 
-        if curve not in self.curve:
-            logging.error('{} data not in graph. Current graphs: {}'.format(curve, ','.join(self.curve)))
-            return
-
-        if show:
-            self.legend.addItem(self.curves[curve], curve)
-            self.plt.addItem(self.curves[curve])
-        else:
-            self.legend.removeItem(curve)
-            self.plt.removeItem(self.curves[curve])
+        if curve in self.curves:
+            if show:
+                self.curves[curve].add_to_plot()
+                self.curves[curve].add_to_legend()
+            else:
+                self.curves[curve].remove_from_plot()
+                self.curves[curve].remove_from_legend()
 
 
-class BeamCurrentGraph(pg.PlotWidget):
+class BeamCurrentPlot(pg.PlotWidget):
     pass
 
 
