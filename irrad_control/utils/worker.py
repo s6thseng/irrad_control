ff8482b7f1e8170081609225431222242dc07feb
leloup314 <s6plwolf@uni-bonn.de>
2019-01-17 14:22:04 +0100

    ENH: use QThreadPool and QRunnable for GUI-multithreading

--- irrad_control/utils/worker.py
+++ irrad_control/utils/worker.py
@@ -2,14 +2,16 @@ import traceback
 from PyQt5 import QtCore
 
 
-class Worker(QtCore.QObject):
-    """
-    Implements a worker object on which functions can be executed for multi-threading within Qt.
-    The worker must be moved to a QThread via the QObject.moveToThread() method.
-    """
-
+class WorkerSignals(QtCore.QObject):
+    
     finished = QtCore.pyqtSignal()
     exceptionSignal = QtCore.pyqtSignal(Exception, str)
+
+class Worker(QtCore.QRunnable):
+    """
+    Implements a worker on which functions can be executed for multi-threading within Qt.
+    The worker is an instance of QRunnable, which can be started and handled automatically by Qt and its QThreadPool.
+    """
 
     def __init__(self, func, *args, **kwargs):
         super(Worker, self).__init__()
@@ -20,8 +22,12 @@ class Worker(QtCore.QObject):
         self.args = args
         # Keyword arguments of main function
         self.kwargs = kwargs
-
-    def work(self):
+        
+        # Needs to be done this way since QRunnable cannot emit signals; QObject needed
+        self.signals = WorkerSignals()
+    
+    @QtCore.pyqtSlot()
+    def run(self):
         """
         Runs the function func with given arguments args and keyword arguments kwargs.
         If errors or exceptions occur, a signal sends the exception to main thread.
@@ -37,10 +43,11 @@ class Worker(QtCore.QObject):
             else:
                 self.func()
 
-            self.finished.emit()
-
         except Exception as e:
             # Format traceback and send
             trc_bck = traceback.format_exc()
             # Emit exception signal
-            self.exceptionSignal.emit(e, trc_bck)
+            self.signals.exceptionSignal.emit(e, trc_bck)
+            
+        finally:
+            self.signals.finished.emit()
