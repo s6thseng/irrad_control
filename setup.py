888905aa45d4103106c7845bfdaecbf8c77c08ad
leloup314 <wolf@physik.uni-bonn.de>
2019-08-15 14:11:04 +0200

    ENH: allow setup to be done for server; ADD: requirements_server.txt

--- setup.py
+++ setup.py
@@ -1,30 +1,46 @@
 #!/usr/bin/env python
+import sys
 from setuptools import setup, find_packages  # This setup relies on setuptools since distutils is insufficient and badly hacked code
+
+# Figure out if we're installing on control PC or on server
+try:
+    _server = True if sys.argv[1] == 'server' else False
+    del sys.argv[1]
+except IndexError:
+    _server = False
 
 version = '0.0.1'
 author = 'Pascal Wolf'
 author_email = 'wolf@physik.uni-bonn.de'
 
-with open('requirements.txt') as f:
+with open('requirements.txt' if not _server else 'requirements_server.txt') as f:
     required = f.read().splitlines()
 
-setup(
-    name='irrad_control',
-    version=version,
-    description='Control software for irradiation facility at HISKP cyclotron at Bonn University',
-    url='https://github.com/SiLab-Bonn/irrad_control',
-    license='MIT License',
-    long_description='',
-    author=author,
-    maintainer=author,
-    author_email=author_email,
-    maintainer_email=author_email,
-    packages=find_packages(),
-    setup_requires=['setuptools'],
-    install_requires=required,
-    include_package_data=True,  # accept all data files and directories matched by MANIFEST.in or found in source control
-    package_data={'': ['README.*', 'VERSION'], 'docs': ['*'], 'examples': ['*']},
-    keywords=['radiation damage', 'NIEL', 'silicon', 'irradiation', 'proton', 'fluence'],
-    platforms='any',
-    entry_points={'console_scripts': ['irrad_control = irrad_control.main:main']}
-)
+# Make dict to pass to setup
+setup_kwargs = {'name': 'irrad_control',
+                'version': version,
+                'description': 'Control software for irradiation facility at HISKP cyclotron at Bonn University',
+                'url': 'https://github.com/SiLab-Bonn/irrad_control',
+                'license': 'MIT License',
+                'long_description': '',
+                'author': author,
+                'maintainer': author,
+                'author_email': author_email,
+                'maintainer_email': author_email,
+                'packages': find_packages(),
+                'setup_requires': ['setuptools'],
+                'install_requires': required,
+                'include_package_data': True,  # accept all data files and directories matched by MANIFEST.in or found in source control
+                'package_data': {'': ['README.*', 'VERSION'], 'docs': ['*'], 'examples': ['*']},
+                'keywords':['radiation damage', 'NIEL', 'silicon', 'irradiation', 'proton', 'fluence'],
+                'platforms': 'any',
+                'entry_points': {'console_scripts': ['irrad_control = irrad_control.main:main']}
+                }
+
+# Remove entry_points for server setup
+if _server:
+    del setup_kwargs['entry_points']
+
+# Setup
+setup(**setup_kwargs)
+
