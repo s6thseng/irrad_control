070613462d97baa642b836297e6ae13a20062519
leloup314 <wolf@physik.uni-bonn.de>
2019-08-22 16:53:45 +0200

    ENH: add Arduino temperature sensor interface using Arduino 10 bit ADC and NTC thermistor circuit

---
+++ irrad_control/devices/temp/ArduinoTempSens/ArduinoTempSens.ino
@@ -0,0 +1,108 @@
+/*
+------------------------------------------------------------
+Copyright (c) All rights reserved
+SiLab, Institute of Physics, University of Bonn
+-----------------------------------------------------------
+Using the Arduino Nano as temperature sensor via voltage divider and NTC thermistor.
+Every analog pin is connected to read the voltage over a thermistor, therefore up to 8
+temperature values can be recorded.
+*/
+
+// Define resistance of thermistor at 25 degrees C
+#define NTCNOMINAL 10000
+// Nominal temperature for above resistance (almost always 25 C)
+#define TEMPNOMINAL 25
+// Average each temperature value over 10 analog readS
+#define NSAMPLES 5
+// The beta coefficient of the thermistor (usually 3000-4000)
+#define BETACOEFF 3450
+// Resistivity of the resistors in series to the NTC, forming voltage divider
+#define RESISTOR 10000
+// Kelvin
+#define KELVIN 273.15
+// Array of analog input pins on Arduino
+const int THERMISTORPINS [] = {A0, A1, A2, A3, A4, A5, A6, A7};
+
+
+// Setup
+void setup(void) {
+  // Initialize serial connection
+  Serial.begin(9600);
+
+  // Set 3.3V as external reference voltage instead of internal 5V reference
+  analogReference(EXTERNAL);
+}
+
+// Steinhart-Hart equation for NTC: 1/T = 1/T_0 + 1/B * ln(R/R_0)
+float steinhart_NTC(float r) {
+  float temperature;
+
+  // Do calculation
+  temperature = 1.0 / (1.0 / (TEMPNOMINAL + KELVIN) + 1.0 / BETACOEFF * log(r / NTCNOMINAL));
+
+  // To Kelvin
+  temperature -= KELVIN;
+  
+  return temperature;
+}
+
+// Takes integer analog pin to read temperature from
+float get_temp(int T_PIN) {
+
+  // Store resitance value
+  float ohm = 0;
+
+  float temp_celsius;
+
+  // take N samples in a row, with a slight delay
+  for (int i=0; i< NSAMPLES; i++) {
+    ohm += analogRead(T_PIN);
+    delay(10);
+  }
+
+  // Do the average
+  ohm /= NSAMPLES;
+
+  // Convert  ADC resistance value to resistance in Ohm
+  ohm = 1023 / ohm - 1 ;
+  ohm = RESISTOR / ohm;
+
+  temp_celsius = steinhart_NTC(ohm);
+
+  return temp_celsius;
+}
+
+// Main loop
+void loop(void) {
+  // Needed variables
+  char c;
+  char delimiter = 'T';
+  int temp_pin;
+  float temp_celsius;
+
+  // Get input from serial connection
+  if (Serial.available()) {
+    // Read from serial
+    c = Serial.read();
+
+    // If serial input c is delimiter, which is 'T', it is followed by integer pin number
+    while (c == delimiter) {
+      // Get pin number of analog pin from which temperature should be read
+      temp_pin = Serial.parseInt();
+
+      // We only have 8 analog pins
+      if (0 <= temp_pin && temp_pin < 8) {
+        // Get temperature as degrees Celsius
+        temp_celsius = get_temp(THERMISTORPINS[temp_pin]);
+
+        // Send out, two decimal places
+        Serial.println(temp_celsius, 2);
+      }
+      else {
+        // An Oooopsie happened
+        Serial.println(999);
+       }
+      c = Serial.read();   
+    }
+  }
+}
---
+++ irrad_control/devices/temp/arduino_temp_sens.py
@@ -0,0 +1,57 @@
+"""Class to read from Arduino temperature sensor setup"""
+
+import serial
+import logging
+
+
+class ArduinoTempSens(object):
+
+    def __init__(self, port="/dev/ttyUSB0", baudrate=9600, timeout=5):
+        super(ArduinoTempSens, self).__init__()
+
+        # Make nice serial interface
+        try:
+            self.interface = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
+        except serial.SerialException:
+            logging.error("Could not connect to port {}. Maybe it is used by another process?".format(port))
+
+    def get_temp(self, sensor):
+        """Gets temperature of sensor where 0 <= sensor <= 7 is the physical pin number of the sensor on
+        the Arduino analog pin. Can also be a list of ints."""
+
+        # Make int sensors to list
+        sensor = sensor if isinstance(sensor, list) else [sensor]
+
+        # Create string to send via serial which will return respective temperatures
+        cmd = ''.join(['T{}'.format(s) for s in sensor]).encode()
+
+        # Send via serial interface
+        self.interface.write(cmd)
+
+        # Get result; make sure we get the correct amount of results
+        res = [999] * len(sensor)
+        for i in range(len(sensor)):
+            try:
+                res[i] = float(self.interface.readline().strip())
+            # Timeout of readline returned empty string which cannot be converted to float
+            except ValueError:
+                logging.error("Timeout for reading of temperature sensor {}.".format(sensor[i]))
+
+        # Make return dict
+        temp_data = dict(zip(sensor, res))
+
+        # Check results;
+        for j in range(len(res)):
+            if res[j] == 999:  # 999 is error code from Arduino firmware
+                logging.error("Temperature sensor {} could not be read.".format(sensor[j]))
+                del temp_data[sensor[j]]
+            elif res[j] < -90: # When no NTC thermistor is connected to the circuit, low values are returned
+                logging.warning("Temperature sensor {} reads extremely low temperature. Is the thermistor connected correctly?".format(sensor[j]))
+
+        return temp_data
+
+
+
+
+
+
