a2cfb73b73a02afe0274992d4c95b1163f9f1154
leloup314 <wolf@physik.uni-bonn.de>
2019-10-28 17:07:52 +0100

    ENH: add set_read_only method to GridContainer

--- irrad_control/gui/widgets/util_widgets.py
+++ irrad_control/gui/widgets/util_widgets.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui, QtCore
 from collections import Iterable
 
 
@@ -93,3 +93,56 @@ class GridContainer(QtWidgets.QGroupBox):
     def get_row_count(self):
         """Return number of rows"""
         return self.cnt_row
+
+    def set_read_only(self, read_only=True, omit=None):
+        """Sets all widgets to read only. If they don't have a readOnly-method, they're disabled"""
+
+        omit = omit if isinstance(omit, Iterable) else [omit]
+
+        # Loop over entire grid
+        for row in range(self.cnt_row):
+            for col in range(self.cnt_col[row]):
+                # Get item at row, col
+                item = self.grid.itemAtPosition(row, col)
+
+                # Item is QSpacerItem or 0 (no item)
+                if isinstance(item, QtWidgets.QSpacerItem) or item == 0:
+                    pass
+
+                # Check whether its QLayoutItem or QWidgetItem
+                elif isinstance(item, QtWidgets.QWidgetItem):
+                    # Extract widget and set read_only
+                    _widget = item.widget()
+                    if type(_widget) not in omit:
+                        self._set_widget_read_only(widget=_widget, read_only=read_only)
+
+                elif isinstance(item, QtWidgets.QLayoutItem):
+                    # Loop over layout and disable widgets
+                    _layout = item.layout()
+                    for i in reversed(range(_layout.count())):
+                        # Check whether its a QWidgetItem and not a Spacer etc
+                        if isinstance(_layout.itemAt(i), QtWidgets.QWidgetItem):
+                            _widget = _layout.itemAt(i).widget()
+                            if type(_widget) not in omit:
+                                self._set_widget_read_only(widget=_widget, read_only=read_only)
+                else:
+                    raise TypeError('Item must be either QWidgetItem or QLayoutItem. Found {}'.format(type(item)))
+
+    @staticmethod
+    def _set_widget_read_only(widget, read_only=True):
+        """Set widget to read only. Use widgets setReadOnly-method else just disable"""
+
+        # We don't have to do anything with labels
+        if not isinstance(widget, QtWidgets.QLabel):
+            # Check if we have readOnly method
+            if hasattr(widget, 'setReadOnly'):
+                widget.setReadOnly(read_only)
+            # If not, just disable
+            else:
+                widget.setEnabled(not read_only)
+
+        # Set color palette to indicate status
+        palette = QtGui.QPalette()
+        palette.setColor(QtGui.QPalette.Base, QtCore.Qt.gray if read_only else QtCore.Qt.white)
+        palette.setColor(QtGui.QPalette.Text, QtCore.Qt.darkGray if read_only else QtCore.Qt.black)
+        widget.setPalette(palette)
